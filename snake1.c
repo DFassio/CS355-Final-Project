d25d5718754fde238a3bdcab82a96789e3a0583a
DFassio <103538955+DFassio@users.noreply.github.com>
2022-04-14 17:52:12 -0400

    Update snake1.c

--- snake1.c
+++ snake1.c
@@ -12,14 +12,33 @@ void move_down();
 //x and y values point to the back of the snake
 int x = 0; //represents COLS (x-axis)
 int y = 0; //represents LINES (y-axis)
-
 int snakeLength = 5;
 
 int main(){
+    initscr();
+    clear();
+    curs_set(0);
+    noecho();
+    keypad(stdscr, TRUE);
+    
     start_game();
     move_right();
-//add switch that will take user input and call movement function based on which arrow key was pressed
-
+    
+//switch that will take user input and call movement function based on which arrow key was pressed
+    int ch;
+    switch (ch){
+        case KEY_RIGHT:
+            move_right();
+            break;
+        case KEY_LEFT:
+            move_left();
+            break;
+        case KEY_UP:
+            move_up();
+            break;
+        case KEY_DOWN:
+            move_down();
+    }
 
     getch();
     endwin();
@@ -27,19 +46,16 @@ int main(){
 
 //function that creates border around terminal and creates the initial snake
 void start_game(){
-    initscr();
-        clear();
+    //create border here        
 
-        //create border here        
-
-        //initialize snake in middle of terminal
-        y = LINES/2;
-        x = COLS/2;
-        int temp = x;
-        for (int i = 0; i < snakeLength; i++){   //builds snake from back to front
-            move(y, ++temp);                     //using a temp variable to make sure x and y
-            addstr("~");                         //still point to back of snake
-        }
+    //initialize snake in middle of terminal
+    y = LINES/2;
+    x = COLS/2;
+    int temp = x;
+    for (int i = 0; i < snakeLength; i++){   //builds snake from back to front
+        move(y, ++temp);                     //using a temp variable to make sure x and y
+        addstr("~");                         //still point to back of snake
+    }
 }
 
 void move_right(){
